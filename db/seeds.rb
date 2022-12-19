de35c18f9716f10287a304a709a1f00ac39f6e86
thomas laugier <thomas.laugier.13@gmail.com>
2022-12-19 14:56:35 +0100

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Car.destroy_all
+
+p "Cars are all destroyed"
+p "-----------------------"
+
+p "Creating seeds"
+p "---------------------"
+
+Car1 = Car.create(brand: "Opel", model: "Corsa", price: "125")
+Car2 = Car.create(brand: "Peugeot", model: "508", price: "235")
+Car3 = Car.create(brand: "Renault", model: "Arkana", price: "195")
+Car4 = Car.create(brand: "Mercedes", model: "A45S", price: "346")
+Car5 = Car.create(brand: "Fiat", model: "Multipla", price: "75")
+
+p "Seeds create"
